1d2cf6a21330e7e65706f7175980953290210fe5
Abraham Oni <oniabraham465@gmail.com>
2026-09-15 01:03:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2287,4 +2287,5 @@ init_db()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
